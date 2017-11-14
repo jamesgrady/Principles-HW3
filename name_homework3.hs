83b28d4cd106bd6b037e71916a8b453f179e3fdd
kjf3md <kjf3md@mail.missouri.edu>
2017-11-14 09:37:01 -0600

    prob2 working. Fails second and third test for some reason even though they work if you run them individually.

--- name_homework3.hs
+++ name_homework3.hs
@@ -18,7 +18,21 @@ prob1 x = helper (words x)
 		helper [] = []
 			
 prob2    :: PExp -> Int
-prob2 x = undefined
+prob2 x = helper x []
+	where
+		helper (Val x : xs) stack = helper xs (stack ++ [x])
+		helper _ [] = errorWithoutStackTrace "Bad Input"
+		helper (Plus : xs) stack = helper xs ([solverPlus stack])
+		helper (Mul : xs) stack = helper xs ([solverMul stack])
+		helper (Minus : xs) stack = helper xs ([solverMinus stack])
+		helper (IntDiv : xs) stack = helper xs ([solverIntDiv stack])
+		helper [] stack = solverEnd stack
+		helper _ _ = errorWithoutStackTrace "Bad Input"
+		solverPlus [x , y] = x + y 
+		solverMul [x , y] = x * y
+		solverIntDiv [x , y] = if y == 0 then errorWithoutStackTrace "Cannot divide by zero" else  x `div` y
+		solverMinus [x , y] = x - y
+		solverEnd (x:xs) = x
 
 prob3    :: a
 prob3    = undefined
@@ -41,21 +55,20 @@ test_prob1 = hspec $ do
 		context "For \"4 + 5 * 2\"" $ do
 			it "should return [Val 4, Plus, Val 5, Mul, Val 2]" $ do
 				prob1 "4 + 5 * 2" `shouldBe` [Val 4, Plus, Val 5, Mul, Val 2]
+test_prob2 :: IO()
+test_prob2 = hspec $ do
+	describe "Prob2 from HW3" $ do
+		context "For [Val 4, Val 2, IntDiv]" $ do
+			it "should return 2" $ do
+				prob2 [Val 4, Val 2, IntDiv] `shouldBe` 2
 
--- test_prob2 = hspec $ do
--- 	describe "Prob2 from HW3" $ do
--- 		context "For [Val 4, Val 2, IntDiv]" $ do
--- 			it "should return 2" $ do
--- 				prob2 [Val 4, Val 2, IntDiv] `shouldBe` 2
+		context "For [Mul]" $ do 
+			it "should return \"Exception: Bad Input\"" $ do
+				prob2 [Mul] `shouldBe` errorWithoutStackTrace "Bad Input"
 
--- 		context "For [Mul]" $ do 
--- 			it "should return \"Exception: Bad Input\"" $ do
--- 				prob2 [Mul] `shouldThrow` AnyError
-
--- 		context "For [Val 4, Val 0, IntDiv]" $ do
--- 			it "should return \"Cannot divide by zero!\"" $ do
--- 				prob2 [Val 4, Val 0, IntDiv] `shouldThrow` AnyError
-
+		context "For [Val 4, Val 0, IntDiv]" $ do
+			it "should return \"Cannot divide by zero!\"" $ do
+				prob2 [Val 4, Val 0, IntDiv] `shouldBe` errorWithoutStackTrace "Cannot divide by zero"
 
 
 
